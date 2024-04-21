c01ab721fdb1916e05cb96aa494a169744434f1b
ali-ammar-dow <156888492+ali-ammar-dow@users.noreply.github.com>
2024-04-21 14:41:29 +0300

    Delete account_analatic directory

--- account_analatic/model/account.py
+++
@@ -1,9 +0,0 @@
-from odoo import models,api, fields,_
-
-
-class AccountAnalyticAccount(models.Model):
-    _inherit = 'account.analytic.account'
-    _description = "this module is for testing purpose only, it was created by Eng Ali Ammar"
-
-
-    budget_line  = fields.One2many('crossovered.budget.lines', 'analytic_account_id', 'Budget Lines')
