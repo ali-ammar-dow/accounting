649cf32c36731965705be43195d1a3c2c05038d0
ali-ammar-dow <156888492+ali-ammar-dow@users.noreply.github.com>
2024-06-12 14:08:02 +0300

    Add files via upload

---
+++ bookmark_school/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Product school",
+    "summary": """ """,
+    "version": "0.1",
+    "license": "LGPL-3",
+    "website": "",
+    "depends": ["base", 'web', 'product', 'sale', 'mail'],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/product_product.xml",
+        "views/product_school.xml",
+        "report/reports.xml"
+    ],
+}
---
+++ bookmark_school/models/product_product.py
@@ -0,0 +1,43 @@
+from odoo import models, fields, api
+
+
+class MyModel(models.Model):
+    _inherit = 'product.template'
+
+    school_id = fields.Many2one('product.school', string="School")
+
+
+class MyModelSchool(models.Model):
+    _name = 'product.school'
+    _rec_name = 'name'
+
+    name = fields.Char(string="Name")
+
+
+class MyModelSale(models.Model):
+    _inherit = 'sale.order.line'
+
+    school_id = fields.Many2one(related="product_template_id.school_id")
+
+
+class MyModelSale(models.Model):
+    _inherit = 'sale.order'
+
+    phone = fields.Char(related="partner_id.phone")
+    email = fields.Char(related="partner_id.email")
+    preferred_delivery_date = fields.Html(string="Preferred Delivery Date", compute="compute_preferred_delivery_date")
+
+    def compute_preferred_delivery_date(self):
+        for rec in self:
+            msg = self.env['mail.message'].search([('model', '=', 'sale.order'),
+                                                   ('res_id', '=', rec.id),
+                                                   ('subtype_id', '=', self.env.ref('mail.mt_note').id),
+                                                   ('body', 'ilike', 'Preferred Delivery Date')
+                                                   ], limit=1)
+            if msg:
+                rec.preferred_delivery_date = msg.body
+            else:
+                rec.preferred_delivery_date = None
+
+
+
