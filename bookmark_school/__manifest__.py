6b4324bd2c3fa775cd4e3469f079138fb630a324
ali-ammar-dow <156888492+ali-ammar-dow@users.noreply.github.com>
2024-06-12 15:59:07 +0300

    Add files via upload

--- bookmark_school/__manifest__.py
+++ bookmark_school/__manifest__.py
@@ -4,7 +4,7 @@
     "version": "0.1",
     "license": "LGPL-3",
     "website": "",
-    "depends": ["base", 'web', 'product', 'sale', 'mail' , 'web_sale'],
+    "depends": ["base", 'web', 'product', 'sale', 'mail' , 'website_sale'],
     "data": [
         "security/ir.model.access.csv",
         "views/product_product.xml",
